071bb37650f54c9c435cb0523800b27ca69a1b9c
Sai Dheeraj <saidheeraj.kumar04@gmail.com>
2019-03-17 11:39:17 +0530

    Revert "Adding basic stadium apis"
    This reverts commit 37fa58fb8a7c2251a2c722f452b5a135ef67a192.

--- cricgod/src/main/java/com/application/cricgod/stadium/service/StadiumService.java
+++
@@ -1,9 +0,0 @@
-package com.application.cricgod.stadium.service;
-
-import com.application.cricgod.util.CustomJsonUtil;
-
-public interface StadiumService {
-	CustomJsonUtil getAllStadiums();
-	CustomJsonUtil getStadium(int stadium_id);
-
-}
